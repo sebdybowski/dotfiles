16c5a20910a06fa96c6b9033069077fe31f108c5
Seb Dybowski <dybowski.seb@gmail.com>
2022-08-11 21:20:32 +0200

    git scripts improvements

--- git.sh
+++ git.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
 
-alias gsb="git add -A && git commit -m $1 && git push origin HEAD && echo '[sd] Pulled changes!'"
-alias gcm="git add -A && git commit -m $1"
-alias grb="git rebase -i HEAD~$1"
-alias gnb="git checkout -b $1"
+gsb() {
+    git add -A
+    git commit -m $1
+    git push origin HEAD
+    echo '[sd] Submitted changes!'
+}
+
+alias gcm="git add -A && git commit -m $($1)"
+alias grb="git rebase -i HEAD~$($1)"
+alias gnb="git checkout -b $($1)"
+
+alias g="echo $($1)"
