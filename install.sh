84e594c6dbe37654ac321c2d7bbe20dce0869765
Seb Dybowski <dybowski.seb@gmail.com>
2022-08-12 15:44:54 +0200

    install script working

--- install.sh
+++ install.sh
@@ -4,22 +4,25 @@ DIR=$(dirname "$0")
 
 source "${DIR}/colors.sh"
 
-SD="$HOME/.sd"
-INIT="$HOME/.sd/init.sh"
-BASHRC="$HOME/.bashrc"
-ZSHRC="$HOME/.zshrc"
+__SD="$HOME/.sd"
+__INIT="$HOME/.sd/init.sh"
+__BASHRC="$HOME/.bashrc"
+__ZSHRC="$HOME/.zshrc"
 
 if
-    test -d "${SD}"
+    test -d "${__SD}"
 then
-    echo -e "${RED}Error: ${SD} already exists. \n Please remove it before installing.${NOCOLOR}"
+    echo -e "${RED}Error: ${__SD} already exists. \n Please remove it before installing.${NOCOLOR}"
     exit 1
 else
-    git clone --recursive https://github.com/sebdybowski/dotfiles.git $SD
+    git clone --recursive https://github.com/sebdybowski/dotfiles.git $__SD &&
+    echo -e "${GREEN}Repository cloned.${NOCOLOR}" &&
 
-    test -f "${BASHRC}" &&
-    echo "source ${INIT}" >> "${BASHRC}"
+    test -f "${__BASHRC}" &&
+    echo "source ${__INIT@Q}" >> "${__BASHRC}" &&
+    echo -e "${BLUE}Added ${__INIT} to ${__BASHRC}.${NOCOLOR}"
 
-    test -f "${ZSHRC}" &&
-    echo "source ${INIT}" >> "${ZSHRC}"
+    test -f "${__ZSHRC}" &&
+    echo "source ${__INIT@Q}" >> "${__ZSHRC}" &&
+    echo -e "${BLUE}Added ${__INIT} to ${__ZSHRC}.${NOCOLOR}"
 fi
