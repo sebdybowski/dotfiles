887ad5e31ede2aeca11fdd8efb4dc4590952e70c
Seb Dybowski <dybowski.seb@gmail.com>
2022-08-12 08:57:58 +0200

    current state

--- install.sh
+++ install.sh
@@ -5,20 +5,21 @@ DIR=$(dirname "$0")
 source "${DIR}/colors.sh"
 
 SD="$HOME/.sd"
-PATH="$HOME/.sd/init.sh"
+INIT="$HOME/.sd/init.sh"
 BASHRC="$HOME/.bashrc"
 ZSHRC="$HOME/.zshrc"
 
 if
     test -d "${SD}"
 then
-    echo -e "${ORANGE}${SD} already exists. \n Please remove it before installing.${NOCOLOR}"
+    echo -e "${RED}Error: ${SD} already exists. \n Please remove it before installing.${NOCOLOR}"
+    exit 1
 else
-    git clone --recursive https://github.com/sebdybowski/dotfiles.git $SD &&
+    git clone --recursive https://github.com/sebdybowski/dotfiles.git $SD
 
     test -f "${BASHRC}" &&
-    echo "source ${PATH}" >> "${BASHRC}"
+    echo "source ${INIT}" >> "${BASHRC}"
 
     test -f "${ZSHRC}" &&
-    echo "source ${PATH}" >> "${ZSHRC}"
+    echo "source ${INIT}" >> "${ZSHRC}"
 fi
